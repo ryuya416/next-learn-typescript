3cc13fbb6d47e18808fa4207b92d95a87ec9cc0a
ryuya0416 <67462677+ryuya416@users.noreply.github.com>
2022-04-05 23:21:14 +0900

    🏷️ Next.jsチュートリアルをTypeScript化

---
+++ components/date.tsx
@@ -0,0 +1,11 @@
+import { parseISO, format } from "date-fns";
+import { VFC } from "react";
+
+type Props = { dateString: string };
+
+const Date: VFC<Props> = ({ dateString }) => {
+  const date = parseISO(dateString);
+  return <time dateTime={dateString}>{format(date, "LLLL d, yyyy")}</time>;
+};
+
+export default Date;
